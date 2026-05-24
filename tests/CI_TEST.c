c2cdc84361dc517c5c10a2183ab40268bdf3c670
miyabi_no1_fan <222554062+sleepy-kernel@users.noreply.github.com>
2026-05-24 20:38:18 +0700

    fix: reduce thread spamming so valgrind won't take too long to run

--- tests/CI_TEST.c
+++ tests/CI_TEST.c
@@ -77,7 +77,7 @@ struct thread_safety_test_arg {
 
 void* thread_safety_test_task(void* arg) {
 
-    enum {spam = 1};
+    enum {spam = 4};
 
     for (uint16_t i = 0; i < spam; i++) {
 
@@ -129,7 +129,7 @@ void* thread_safety_test_task(void* arg) {
 int thread_safety_test(void) {
     const char out_paths_prefix[] = "CI_TEST-%02hu.png";
 
-    enum {thread_count = 50};
+    enum {thread_count = 12};
     pthread_t threads[thread_count] = {0};
     struct thread_safety_test_arg thread_arg[thread_count] = {0};
     char out_paths_ptr[thread_count][256] = {0};
