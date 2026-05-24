9ee494e82497e4c749c687f2a7b2847c072c6688
miyabi_no1_fan <222554062+sleepy-kernel@users.noreply.github.com>
2026-05-25 05:28:34 +0700

    build_lib.sh can pass arg to gcc especially to pass -mno-avx512f in the CI test because valgrind don't support avx512

--- _scripts/archlinux/build_lib.sh
+++ _scripts/archlinux/build_lib.sh
@@ -5,13 +5,13 @@ set -e
 mkdir -p build
 
 echo "=> compiling src files.."
-gcc -c src/slp_image_transform/slp_image_transform.c -O3 -march=native -mtune=native -DNDEBUG -I include -o build/slp_image_transform.o
-gcc -c src/slp_png_read/slp_png_read_from_file.c -O3 -march=native -mtune=native -DNDEBUG -I include -o build/slp_png_read_from_file.o
-gcc -c src/slp_png_write/slp_png_write_to_file.c -O3 -march=native -mtune=native -DNDEBUG -I include -o build/slp_png_write_to_file.o
+gcc -c src/slp_image_transform/slp_image_transform.c -O3 -march=native -mtune=native -DNDEBUG "$@" -I include -o build/slp_image_transform.o
+gcc -c src/slp_png_read/slp_png_read_from_file.c -O3 -march=native -mtune=native -DNDEBUG "$@" -I include -o build/slp_png_read_from_file.o
+gcc -c src/slp_png_write/slp_png_write_to_file.c -O3 -march=native -mtune=native -DNDEBUG "$@" -I include -o build/slp_png_write_to_file.o
 
 echo "=> genrating build/libslp_png.so..."
 # DYNAMIC LINKING:
-gcc -shared build/slp_image_transform.o build/slp_png_read_from_file.o build/slp_png_write_to_file.o -DNDEBUG -lz -flto -o build/libslp_png.so
+gcc -shared build/slp_image_transform.o build/slp_png_read_from_file.o build/slp_png_write_to_file.o -DNDEBUG -flto "$@" -lz -o build/libslp_png.so
 
 echo "=> genrating build/libslp_png.a..."
 # STATIC LINKING:
--- _scripts/archlinux/run.sh
+++ _scripts/archlinux/run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -e
 
-_scripts/archlinux/build_lib.sh
+_scripts/archlinux/build_lib.sh "$@"
 _scripts/archlinux/build_exe.sh "$@"
 echo "Running $3...
 "
