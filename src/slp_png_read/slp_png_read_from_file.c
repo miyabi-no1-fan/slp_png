f80dbd99fc8c84888bf761052e37d37d0b71b6ef
miyabi_no1_fan <222554062+sleepy-kernel@users.noreply.github.com>
2026-05-24 16:27:28 +0700

    fix: remove prototype

--- src/slp_png_read/slp_png_read_from_file.c
+++ src/slp_png_read/slp_png_read_from_file.c
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include <slp_png.h>
-#include <slpz.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
