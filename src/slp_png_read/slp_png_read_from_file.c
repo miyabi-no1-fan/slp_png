bcec54ea27d6bcd35651216693011c4ed9467dec
miyabi_no1_fan <222554062+sleepy-kernel@users.noreply.github.com>
2026-05-30 19:27:05 +0700

    just add a single new line to create more gap between functions

--- src/slp_png_read/slp_png_read_from_file.c
+++ src/slp_png_read/slp_png_read_from_file.c
@@ -1177,6 +1177,7 @@ static inline void slp_png_colortype3_unpack(uint8_t* restrict buffer, struct sl
 }
 
 
+
 static inline void slp_png_index_u32_to_RGBA(struct slp_image* restrict slp_png_stream, const uint8_t* restrict palette) {
     for (size_t i = 0; i + slp_png_stream->channels <= slp_png_stream->image_size; i+=slp_png_stream->channels) {
         int index = slp_png_stream->buffer[i] * slp_png_stream->channels;
