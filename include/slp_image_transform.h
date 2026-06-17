e15cf71545607bd251db3d1b896551d5c8d9d5d2
slp-c <222554062+slp-c@users.noreply.github.com>
2026-06-17 16:05:30 +0700

    + use slp_image_t instead of struct slp_image + comment out testing functions + new function slp_image_fill

--- include/slp_image_transform.h
+++ include/slp_image_transform.h
@@ -1,22 +1,19 @@
 #pragma once
 #include <stdint.h>
 #include <stdbool.h>
-#include <slp_png.h>
+#include <slp_image.h>
 
-
-bool slp_image_convert_to_8bit(struct slp_image* image);
-bool slp_image_convert_to_16bit(struct slp_image* image);
-
-bool slp_image_crop(struct slp_image* image, const uint32_t new_width, const uint32_t new_height, const uint32_t offset_width, const uint32_t offset_height);
-
-bool slp_image_linear_transform(struct slp_image* restrict image, const double* restrict A, const uint8_t* restrict background); // A[4] = a00, a01, a10, a11
-
-bool slp_image_convert_G8_to_RGBA32(struct slp_image* image);
-bool slp_image_convert_GA16_to_RGBA32(struct slp_image* image);
-bool slp_image_convert_G16_to_RGBA64(struct slp_image* image);
-bool slp_image_convert_GA32_to_RGBA64(struct slp_image* image);
-
-struct slp_image slp_formatted_image_copy(const struct slp_image image);
-
-bool slp_image_unformat(struct slp_image* image);
-bool slp_image_format(struct slp_image* image);
+bool slp_image_convert_to_8bit(slp_image_t* image);
+bool slp_image_convert_to_16bit(slp_image_t* image);
+bool slp_image_crop(slp_image_t* image, const uint32_t new_width, const uint32_t new_height, const uint32_t offset_width, const uint32_t offset_height);
+void slp_image_fill(uint8_t* buffer, const size_t buffer_size, const uint8_t* pixel, const uint8_t pixel_size);
+bool slp_image_linear_transform(slp_image_t* restrict image, const double* restrict A, const uint8_t* restrict background); // A[4] = a00, a01, a10, a11
+/*
+bool slp_image_convert_G8_to_RGBA32(slp_image_t* image);
+bool slp_image_convert_GA16_to_RGBA32(slp_image_t* image);
+bool slp_image_convert_G16_to_RGBA64(slp_image_t* image);
+bool slp_image_convert_GA32_to_RGBA64(slp_image_t* image);
+*/
+slp_image_t slp_image_copy(const slp_image_t image);
+bool slp_image_unformat(slp_image_t* image);
+bool slp_image_format(slp_image_t* image);
