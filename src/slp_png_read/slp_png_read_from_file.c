332f9fb179e7bc00f41d4cbfe8b4949fa589a3ed
miyabi-no1-fan <222554062+miyabi-no1-fan@users.noreply.github.com>
2026-08-03 18:02:38 +0700

    change the owner username

--- src/slp_png_read/slp_png_read_from_file.c
+++ src/slp_png_read/slp_png_read_from_file.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2026 slp-c
+Copyright 2026 miyabi-no1-fan
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
