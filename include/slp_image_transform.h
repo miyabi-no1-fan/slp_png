00cb10acbc5b3a24e3839210d9321318b6668846
miyabi-no1-fan <222554062+miyabi-no1-fan@users.noreply.github.com>
2026-08-05 10:41:39 +0700

    - merge slp_image.h into slp_png.h - disable slp_image_transform.h with `#define SLP_IMAGE_TRANSFROM_RELEASE 0`

--- include/slp_image_transform.h
+++ include/slp_image_transform.h
@@ -1,27 +1,31 @@
+#define SLP_IMAGE_TRANSFROM_RELEASE 0  // not release yet, we didn't have any test for it at all
+
+#if SLP_IMAGE_TRANSFROM_RELEASE
+
 #pragma once
-#include <slp_image.h>
+#include <slp_png.h>
 #include <stdbool.h>
-#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-bool slp_image_convert_to_8bit(slp_image_t* image);
+void slp_image_convert_to_8bit(slp_image_t* image);
 bool slp_image_convert_to_16bit(slp_image_t* image);
+void slp_image_bswap16(slp_image_t* image);
+
 bool slp_image_crop(slp_image_t* image, const uint32_t new_width, const uint32_t new_height, const uint32_t offset_width, const uint32_t offset_height);
+
 void slp_image_fill(uint8_t* buffer, const size_t buffer_size, const uint8_t* pixel, const uint8_t pixel_size);
-bool slp_image_linear_transform(slp_image_t* restrict image, const double* restrict A, const uint8_t* restrict background);  // A[4] = a00, a01, a10, a11
-/*
-bool slp_image_convert_G8_to_RGBA32(slp_image_t* image);
-bool slp_image_convert_GA16_to_RGBA32(slp_image_t* image);
-bool slp_image_convert_G16_to_RGBA64(slp_image_t* image);
-bool slp_image_convert_GA32_to_RGBA64(slp_image_t* image);
-*/
 slp_image_t slp_image_copy(const slp_image_t image);
-bool slp_image_unformat(slp_image_t* image);
-bool slp_image_format(slp_image_t* image);
+
+bool slp_image_linear_transform(slp_image_t* restrict image, const double* restrict A, const uint8_t* restrict background);  // A[4] = a00, a01, a10, a11
+
+bool slp_image_pack(slp_image_t* image);
+bool slp_image_unpack(slp_image_t* image);
 
 #ifdef __cplusplus
 }
+#endif
+
 #endif
