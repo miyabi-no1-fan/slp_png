185164aa34abe5f53d0bf5bdcb4537474e80b53d
miyabi-no1-fan <222554062+miyabi-no1-fan@users.noreply.github.com>
2026-08-07 07:04:42 +0700

    Potential deref NULL ptr in `slp_png_write` (#42)
    * `image.pixels` could be NULL
    `slp_png_write` must check for that and return an error
    
    * also validate image.image_size

--- src/write.c
+++ src/write.c
@@ -25,7 +25,7 @@ static uint8_t get_color_type(const uint8_t channels);
 extern int encode(slp_image_t* restrict image, FILE* restrict file);
 
 int slp_png_write(slp_image_t image, const char* path) {
-    if (image.height == 0 || image.width == 0 || image.channels == 0) return INVALID_PNG;
+    if (image.pixels == NULL || image.height == 0 || image.width == 0 || image.channels == 0) return INVALID_PNG;
     switch (image.bit_depth) {
         case 1: break;
         case 2: break;
@@ -34,6 +34,8 @@ int slp_png_write(slp_image_t image, const char* path) {
         case 16: break;
         default: return INVALID_PNG;
     }
+    if (image.image_size != image.height * div_ceil((size_t)image.width * image.channels * image.bit_depth, 8))
+        return INVALID_PNG;
 
     const uint16_t random_value_for_edian_test = 1;
     const bool is_little_edian = *(uint8_t*)(&random_value_for_edian_test);
@@ -81,7 +83,7 @@ int slp_png_write(slp_image_t image, const char* path) {
         fwrite(&crc, 1, 4, file) != 4)
     {
         fclose(file);
-        return INVALID_PNG;
+        return IO_ERR;
     }
 
     int ret = encode(&image, file);
