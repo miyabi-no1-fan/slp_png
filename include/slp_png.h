b1a74200b24ee641476369a4383c9b7aebf279de
miyabi-no1-fan <222554062+miyabi-no1-fan@users.noreply.github.com>
2026-08-22 01:17:04 +0700

    Fix bug (#67)
    * change `channels` field in `slp_image_t` to `uint8_t` instead of `uint32_t`
    
    * BUG: `get_channels` return 0 on invalid `bit_depth` and -1 on invalid `color_type`. We only check for case `channels == -1` but forgot case `channels == 0`.
    fix: `get_channels` return 0 on both case, and we'll check for that
    
    * a typo when using `SLP_FREE`, the size parameter got wrong
    
    * In `SLP_FREE` use `(void)(size)` to ignore the `size` parameter.
    This way the intellisense still work to prevent typos errors.

--- include/slp_png.h
+++ include/slp_png.h
@@ -26,7 +26,7 @@ typedef struct slp_image_t {
     uint8_t* pixels;
     uint32_t height;
     uint32_t width;
-    uint32_t channels;
+    uint8_t channels;
     uint8_t bit_depth;
     size_t image_size;
 } slp_image_t;
@@ -118,7 +118,7 @@ you'll have to change the definition **before compile** `slp_png`'s source code.
     #endif
 
     #ifndef SLP_FREE
-        #define SLP_FREE(ptr, size) free(ptr)
+        #define SLP_FREE(ptr, size) do { (void)(size); free(ptr); } while(0)
     #endif
 
     #ifndef SLP_MEMCPY
